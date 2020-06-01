31463b22650b912479ded69a3cccb7f18ad32a3c
Jim <1150660744@qq.com>
2020-06-02 03:13:43 +0800

    add annotation
    Signed-off-by: Jim <1150660744@qq.com>

--- pt.h
+++ pt.h
@@ -7,12 +7,18 @@
 	__FILE__, __func__, __LINE__); \
 	printf(format, ##__VA_ARGS__);}
 
+/*protothread status*/
 #define BEGIN 0
 #define YIELD 1
 #define BLOCK 2
 
+/*the position of label*/
 #define POS(token) pos_##token
 
+/*for debug info*/
+/*#pragma message(POS(token))*/
+
+/*protothread start*/
 #define PT_INIT(event) \
 	static int token;\
 	switch(event) { \
@@ -23,20 +29,23 @@
 	default: \
 		break; \
 	}
-
+/*prothread end*/
 #define PT_DEINIT()\
 	POS(token):
 
+/*protothread wait*/
 #define PT_BLOCK() \
 	srand(time(NULL)); \
 	token = rand();\
 	return 0; \
 	POS(token):
 
+/*protothread resume*/
 #define PT_RESUME(p) \
 	int e = YIELD; \
 	p(&e);
 
+/*prothread create*/
 #define PT_CREATE(p, arg) \
 	if (p == NULL) { \
 		return 0; \
