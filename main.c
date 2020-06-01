bbac742bc9280fe25efb141d8b443e71a6c6824e
Jim <1150660744@qq.com>
2020-06-01 13:05:19 +0800

    use macro to cutdown code
    Signed-off-by: Jim <1150660744@qq.com>

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+typedef void (*PHREAD_ENTRY) (void *arg);
+
 #define print(format, ...) \
 	{printf("[%s : %s : %d] ", \
 	__FILE__, __func__, __LINE__); \
@@ -9,7 +11,24 @@
 #define YIELD 1
 #define BLOCK 2
 
-typedef void (*PHREAD_ENTRY) (void *arg);
+#define PT_INIT(event) \
+	switch(event) { \
+	case YIELD: \
+		goto pos; \
+	case BEGIN: \
+		break;\
+	default: \
+		break; \
+	} \
+
+#define PT_BLOCK() \
+	return 0; \
+	pos: \
+
+#define PT_RESUME(p) \
+	int event = YIELD; \
+	p(&event); \
+
 
 int resume_phread(PHREAD_ENTRY p)
 {
@@ -32,28 +51,16 @@ int create_phread(PHREAD_ENTRY p, void *arg)
 int phread_one(void *arg)
 {
 	int event = *((int*)arg);
-	switch(event) {
-	case YIELD:
-		goto pos;
-	case BEGIN:
-		break;
-	default:
-		break;
-	}
-	int pos = 0;
-	//block_phread(phread_one);
-	event = BLOCK;
-	if (event == BLOCK) {
-		print("block\n");
-		return 0;
-	}
-pos:
+	PT_INIT(event);
+	print();
+	PT_BLOCK();
+
 	print("running\n");
 }
 
 int phread_two(void *arg)
 {
-	resume_phread(phread_one);
+	PT_RESUME(phread_one);
 	print("running\n");
 }
 
