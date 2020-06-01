5ab2a84fbbfa644573b71c478eff01e1e5dc3997
Jim <1150660744@qq.com>
2020-06-01 12:13:22 +0800

    basic frame
    Signed-off-by: Jim <1150660744@qq.com>

--- main.c
+++ main.c
@@ -5,14 +5,40 @@
 	__FILE__, __func__, __LINE__); \
 	printf(format, ##__VA_ARGS__);}
 
-int phread_one()
+typedef void (*PHREAD_ENTRY) (void *arg);
+
+int resume_phread(void *p)
 {
-	print("running\n");
+
 }
 
-int phread_two()
+int block_phread(void *p)
 {
-	print("running\n");
+
+}
+
+int create_phread(PHREAD_ENTRY p, void *arg)
+{
+	if (p == NULL) {
+		print("pthread null\n");
+	}
+	p(arg);
+}
+
+int phread_one(void *arg)
+{
+	//while (1) {
+		//if (condition == 1) {
+			print("running\n");
+		//}
+	//}
+}
+
+int phread_two(void *arg)
+{
+	//while (1) {
+		print("running\n");
+	//}
 }
 
 int main()
@@ -28,5 +54,7 @@ int main()
 		printf("line: %d\n", line);
 		break;
 	}
+	create_phread(phread_one, NULL);
+	create_phread(phread_two, NULL);
 	return 0;
 }
