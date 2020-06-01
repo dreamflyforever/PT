81ddd5554e573549dfee1dae54d6a2a88705049a
Jim <1150660744@qq.com>
2020-06-01 13:19:23 +0800

    example for PT
    Signed-off-by: Jim <1150660744@qq.com>

--- main.c
+++ main.c
@@ -1,39 +1,4 @@
-#include <stdio.h>
-
-typedef void (*PHREAD_ENTRY) (void *arg);
-
-#define print(format, ...) \
-	{printf("[%s : %s : %d] ", \
-	__FILE__, __func__, __LINE__); \
-	printf(format, ##__VA_ARGS__);}
-
-#define BEGIN 0
-#define YIELD 1
-#define BLOCK 2
-
-#define PT_INIT(event) \
-	switch(event) { \
-	case YIELD: \
-		goto pos; \
-	case BEGIN: \
-		break;\
-	default: \
-		break; \
-	}
-
-#define PT_BLOCK() \
-	return 0; \
-	pos:
-
-#define PT_RESUME(p) \
-	int event = YIELD; \
-	p(&event);
-
-#define PT_CREATE(p, arg) \
-	if (p == NULL) { \
-		return 0; \
-	} \
-	p(arg);
+#include "pt.h"
 
 int phread_one(void *arg)
 {
