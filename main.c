c29f3a6914c21714f5d5b286ec13a306e8c366a4
Jim <1150660744@qq.com>
2020-06-03 00:46:00 +0800

    add another implementation model
    Signed-off-by: Jim <1150660744@qq.com>

--- main.c
+++ main.c
@@ -1,24 +1,61 @@
-#include "pt.h"
+#include <stdio.h>
+
+#define print(format, ...) \
+	{printf("[%s : %s : %d] ", \
+	__FILE__, __func__, __LINE__); \
+	printf(format, ##__VA_ARGS__);}
+
+#define PT_CREATE(p, arg) \
+	if (p == NULL) { \
+		return 0; \
+	} \
+	p(arg);
+
+#define BEGIN 0
+#define YIELD 1
+#define BLOCK 2
+
+#define PT_INIT(event) \
+	switch (event) \
+	{\
+	case BEGIN: \
+	{
+
+#define PT_BLOCK() \
+	}\
+	case BLOCK: \
+		return 0; \
+	case YIELD: \
+	{\
+
+#define PT_DEINIT() \
+	}\
+		break; \
+	}
+
+#define PT_RESUME(p) \
+	int _e = YIELD;\
+	p(&_e);
 
 int phread_one(void *arg)
 {
-	int event = *((int*)arg);
-	PT_INIT(event);
-	print("blocking\n");
+	int e = *((int *)arg);
+	PT_INIT(e);
+	print("block\n")
 	PT_BLOCK();
-	print("running\n");
+	print("run\n");
+	PT_DEINIT();
 	return 0;
 }
 
 int phread_two(void *arg)
 {
-	int event = *((int*)arg);
-	PT_INIT(event);
-	print("running\n");
+	int e = *((int *)arg);
+	PT_INIT(e);
+	print("run\n");
 	PT_RESUME(phread_one);
 	print("resume\n");
 	PT_DEINIT();
-	return 0;
 }
 
 int main()
