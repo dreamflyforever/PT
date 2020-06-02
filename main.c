0e9abfba32c976ae507c5c2ac604ef88eaa26fab
Jim <1150660744@qq.com>
2020-06-03 01:07:03 +0800

    change pt.h to pt_label.h
    Signed-off-by: Jim <1150660744@qq.com>

--- main.c
+++ main.c
@@ -1,51 +1,16 @@
 #include <stdio.h>
+#include "pt_label.h"
+#include "pt_switch.h"
 
 #define print(format, ...) \
 	{printf("[%s : %s : %d] ", \
 	__FILE__, __func__, __LINE__); \
 	printf(format, ##__VA_ARGS__);}
 
-#define PT_CREATE(p, arg) \
-	if (p == NULL) { \
-		return 0; \
-	} \
-	p(arg);
-
-#define BEGIN 0
-#define BLOCK 2
-
-#define PT_INIT() \
-	static int event = BEGIN;\
-	switch (event) \
-	{\
-	case BEGIN: \
-	{
-
-/*notic: use `\` means on the one line*/
-#define PT_BLOCK() \
-	}\
-		event = __LINE__;\
-		break; \
-	case __LINE__: \
-	{\
-
-#define PT_DEINIT() \
-	}\
-		break; \
-	default:\
-		print("event: %d\n", event); \
-		break;\
-	}
-
-#define PT_RESUME(p) \
-	p(NULL);
-
 int phread_one(void *arg)
 {
 	PT_INIT();
 	print("block\n")
-	PT_BLOCK();
-	print("run\n");
 	PT_BLOCK();
 	print("run\n");
 	PT_DEINIT();
@@ -56,8 +21,6 @@ int phread_two(void *arg)
 {
 	PT_INIT();
 	print("run\n");
-	PT_RESUME(phread_one);
-	print("resume\n");
 	PT_RESUME(phread_one);
 	print("resume\n");
 	PT_DEINIT();
